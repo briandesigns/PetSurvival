7e756d0ebc428d0d6adbed76def3848e6ef17388
Joe Peplowski <jumpeplowski@gmail.com>
2016-03-04 17:14:22 -0500

    Array of map tile data gets transformed into the game map

--- src/play/MapLayer.js
+++ src/play/MapLayer.js
@@ -14,22 +14,32 @@ var MapLayer = cc.Layer.extend({
     init:function () {
         this._super();
 
-        //this string represents .tmx tiled map data
-        var mapAsString0 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<map version=\"1.0\" orientation=\"orthogonal\" renderorder=\"right-down\" width=\"5\" height=\"5\" tilewidth=\"32\" tileheight=\"32\" nextobjectid=\"1\">\r\n <tileset firstgid=\"1\" name=\"terrain\" tilewidth=\"32\" tileheight=\"32\" tilecount=\"192\" columns=\"16\">\r\n  <image source=\"HF1_A2.png\" width=\"512\" height=\"384\"\/>\r\n <\/tileset>\r\n <layer name=\"Tile Layer 1\" width=\"5\" height=\"5\">\r\n  <data encoding=\"csv\">\r\n69,70,71,72,73,\r\n85,86,87,88,89,\r\n101,102,103,104,105,\r\n117,118,119,120,121,\r\n133,134,135,136,137\r\n<\/data>\r\n <\/layer>\r\n<\/map>\r\n";
+        //build our array of map tiles
+        var mapArray = [];
+        for (i=0; i<4096; i++) {
+            //for now: set tile i to a random tile
+            mapArray.push(Math.random()*483 + 1);
+        }
+        var mapArrayString = mapArray.toString();
 
-        //create 25 tiled maps
-        for (i=0; i<25; i++) {
-            var map = new cc.TMXTiledMap.create(mapAsString0, "res/map");
+        //add header and footer to build .tmx string
+        var header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<map version=\"1.0\" orientation=\"orthogonal\" renderorder=\"right-down\" width=\"64\" height=\"64\" tilewidth=\"32\" tileheight=\"32\" nextobjectid=\"1\">\r\n <tileset firstgid=\"1\" name=\"terrain\" tilewidth=\"32\" tileheight=\"32\" tilecount=\"483\" columns=\"21\">\r\n  <image source=\"terrain.jpg\" width=\"672\" height=\"736\"\/>\r\n <\/tileset>\r\n <layer name=\"Tile Layer 1\" width=\"64\" height=\"64\">\r\n  <data encoding=\"csv\">\r\n";
+        var footer = "<\/data>\r\n <\/layer>\r\n<\/map>\r\n";
+        var mapAsTmxString = header.concat(mapArrayString, footer);
+
+        //create 16 tiled maps
+        for (i=0; i<16; i++) {
+            var map = new cc.TMXTiledMap.create(mapAsTmxString, "res/map");
             this.maps.push(map);
         }
 
         this.mapWidth = this.maps[0].getContentSize().width;
         this.mapHeight = this.maps[0].getContentSize().height;
 
-        //set the position of 25 tiled maps in a 5x5 grid
-        for (i=0; i<25; i++) {
-            var xPosition = (this.mapWidth * i) % (this.mapWidth * 5);
-            var yPosition = this.mapHeight * parseInt(i / 5);
+        //set the position of 16 tiled maps in a 4x4 grid
+        for (i=0; i<16; i++) {
+            var xPosition = (this.mapWidth * i) % (this.mapWidth * 4);
+            var yPosition = this.mapHeight * parseInt(i / 4);
 
             this.maps[i].setPosition(cc.p(xPosition, yPosition));
             this.addChild(this.maps[i]);
--- src/resource.js
+++ src/resource.js
@@ -5,8 +5,7 @@ var res = {
     menu_button_normal_runaway : "res/menu/menu_button_normal_runaway.png",
     menu_button_selected_runaway: "res/menu/menu_button_selected_runaway.png",
     character_bunny_png : "res/character/bunny.png",
-    trees_png : "res/map/trees.png",
-    HF1_A2_png : "res/map/HF1_A2.png"
+    terrain_jpg : "res/map/terrain.jpg"
 };
 
 var g_resources = [];
