a672366f937147c69942ffdce5418d8907f6eca8
Joe Peplowski <jumpeplowski@gmail.com>
2016-03-04 19:13:31 -0500

    Grid of tiled maps now act as one complete game map (by properly showing map data from a single array)

--- src/play/MapLayer.js
+++ src/play/MapLayer.js
@@ -14,22 +14,39 @@ var MapLayer = cc.Layer.extend({
     init:function () {
         this._super();
 
-        //build our array of map tiles
-        var mapArray = [];
-        for (i=0; i<4096; i++) {
-            //for now: set tile i to a random tile
-            mapArray.push(Math.random()*483 + 1);
+        //the input array of map tiles. for now make this iterate through the tile types
+        var inputTileData = [];
+        for (i=0; i<65536; i++) {
+            inputTileData.push(parseInt(i/483));
         }
-        var mapArrayString = mapArray.toString();
 
-        //add header and footer to build .tmx string
+        //an array of empty tiled map data arrays
+        var tiledMapData = new Array(16);
+        for (var i = 0; i<16; i++) {
+            tiledMapData[i] = [];
+        }
+
+        //fill the tiled map data arrays with the proper data from the input array
+        for (var j=0; j<65536; j++) {
+            var row = parseInt((j%256)/64);
+            var column = parseInt((j/256)/64);
+            var array = column*4 + row;
+            tiledMapData[array].push(inputTileData[j]);
+        }
+
         var header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n<map version=\"1.0\" orientation=\"orthogonal\" renderorder=\"right-down\" width=\"64\" height=\"64\" tilewidth=\"32\" tileheight=\"32\" nextobjectid=\"1\">\r\n <tileset firstgid=\"1\" name=\"terrain\" tilewidth=\"32\" tileheight=\"32\" tilecount=\"483\" columns=\"21\">\r\n  <image source=\"terrain.jpg\" width=\"672\" height=\"736\"\/>\r\n <\/tileset>\r\n <layer name=\"Tile Layer 1\" width=\"64\" height=\"64\">\r\n  <data encoding=\"csv\">\r\n";
         var footer = "<\/data>\r\n <\/layer>\r\n<\/map>\r\n";
-        var mapAsTmxString = header.concat(mapArrayString, footer);
+        var mapAsTmxStrings = [];
 
-        //create 16 tiled maps
+        //attach headers and footers to our arrays to form tmx strings
+        for (var k=0; k<16; k++) {
+            var mapArrayAsString = tiledMapData[k].toString();
+            mapAsTmxStrings.push(header.concat(mapArrayAsString, footer));
+        }
+
+        //create 16 tiled map objects
         for (i=0; i<16; i++) {
-            var map = new cc.TMXTiledMap.create(mapAsTmxString, "res/map");
+            var map = new cc.TMXTiledMap.create(mapAsTmxStrings[i], "res/map");
             this.maps.push(map);
         }
 
