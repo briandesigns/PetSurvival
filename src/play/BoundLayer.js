40b227fa71b4989389316f66e51bd0fbaba3d762
Joe Peplowski <jumpeplowski@gmail.com>
2016-03-15 18:30:08 -0400

    Finished combining collision sprites

--- src/play/BoundLayer.js
+++ src/play/BoundLayer.js
@@ -71,84 +71,84 @@ var BoundLayer = cc.Layer.extend({
             var tile = this.unusedCollisionArrayTiles[i];
             if (tile == 1) {
                 this.checkForBiggerSquare(i, 1, 1);
-
-                var block = new cp.Body(MAX_INT, cp.momentForBox(1, 32, 32));
-                block.setAngVel(0);
-                block.setMoment(MAX_INT);
-                var blockShape = new cp.BoxShape(block, 32, 32);
-                blockShape.setCollisionType(COLLISION_TYPE.bounds);
-                blockShape.setSensor(false);
-                this.space.addBody(block);
-                this.space.addShape(blockShape);
-                block.setPos(cc.p(this.mapLayer.coordinateAtTileIndex(i).x+16, this.mapLayer.coordinateAtTileIndex(i).y+16));
             }
         }
 
     },
+
+    //recursive function that checks if we can include the tiles to the right or below in the area we give as parameters
     checkForBiggerSquare: function(index, width, height){
         var expandRight = true;
         var expandDown = true;
         var fullMapWidth = this.mapLayer.fullMapWidth;
 
-        //console.log("Checking index " + index + " with width: " + width + " and height: " + height);
         //check if we can expand our rectangle to the right
         for (var i=0; i<height*fullMapWidth; i+=fullMapWidth) {
-            console.log();
             if (this.tileRightOf(index+i+(width-1)) == 0){
-                //console.log("tile right of " + (index+i+(width-1)) + " is 0");
                 expandRight = false;
                 break;
             }
         }
         if (expandRight) {
-            //console.log("expand right");
             this.checkForBiggerSquare(index, width+1, height);
         } else {
             //check if we can expand our rectangle down
             for(i=0; i<width; i++) {
                 if (this.tileBelow(index+i+(height-1)*fullMapWidth) == 0) {
-                    //console.log("tile below " + (index+i+(height-1)*fullMapWidth) + " is 0");
                     expandDown = false;
-                    console.log("draw sprite from (" + index%fullMapWidth + "," + parseInt(index/fullMapWidth) + ") to (" + (index%fullMapWidth+width-1) + "," + (parseInt(index/fullMapWidth) + height-1) + ") of size " + (height*width));
+
+                    //we can't expand any farther. draw our sprite at index with width and height
+                    this.drawCollisionSprite(index, width, height);
                     this.removeUsedCollisionArrayTiles(index, width, height);
                     break;
                 }
             }
             if (expandDown) {
-                //console.log("expand down");
                 this.checkForBiggerSquare(index, width, height+1);
             }
         }
-        //console.log("removing (" + (index%fullMapWidth+width-1) + "," + (parseInt(index/fullMapWidth) + height-1) + ")");
-
-        //console.log(parseInt((index+width-1+(height-1)*fullMapWidth)/fullMapWidth));
-
-        //console.log("actually removing (" + (this.mapLayer.coordinateAtTileIndex((index + (width-1) + (height-1)*fullMapWidth)).x/ 32) + "," + (fullMapWidth - this.mapLayer.coordinateAtTileIndex((index + (width-1) + (height-1)*fullMapWidth)).y / 32) + ")");
-        //this.unusedCollisionArrayTiles[(index+width-1+(height-1)*fullMapWidth)] = 0;
     },
 
+    //draw a collision sprite at the given index for the specified width and height
+    drawCollisionSprite: function(index, width, height) {
+        //console.log("draw sprite from (" + index%fullMapWidth + "," + parseInt(index/fullMapWidth) + ") to (" + (index%fullMapWidth+width-1) + "," + (parseInt(index/fullMapWidth) + height-1) + ") of size " + (height*width));
+        var block = new cp.Body(MAX_INT, cp.momentForBox(1, 32*width, 32*height));
+        block.setAngVel(0);
+        block.setMoment(MAX_INT);
+        var blockShape = new cp.BoxShape(block, 32*width, 32*height);
+        blockShape.setCollisionType(COLLISION_TYPE.bounds);
+        blockShape.setSensor(false);
+        this.space.addBody(block);
+        this.space.addShape(blockShape);
+        block.setPos(cc.p(this.mapLayer.coordinateAtTileIndex(index).x+width*16, this.mapLayer.coordinateAtTileIndex(index).y+height*16-(height-1)*32));
+    },
+
+    //remove tiles from our array when we have drawn a collision sprite over them already
     removeUsedCollisionArrayTiles: function(index, width, height) {
         var fullMapWidth = this.mapLayer.fullMapWidth;
 
         for (var i=0; i<width; i++) {
             for (var j=0; j<height; j++) {
-                //console.log("removing (" + (index%fullMapWidth+i) + "," + (parseInt(index/fullMapWidth)+j) + ")");
-                this.unusedCollisionArrayTiles[(index+i+j*fullMapWidth)] = 0;
+                if (this.unusedCollisionArrayTiles[index+i+j*fullMapWidth] != 0) {
+                    this.unusedCollisionArrayTiles[(index+i+j*fullMapWidth)] = 0;
+                }
             }
         }
     },
 
+    //safely returns the tile to the right of the given index
     tileRightOf: function (index) {
         if ((index + 1) % this.mapLayer.fullMapWidth != 0) {
-            return this.mapLayer.collisionArray[index + 1];
+            return this.unusedCollisionArrayTiles[index + 1];
         } else {
             return 0;
         }
     },
 
+    //safely returns the tile below the given index
     tileBelow: function (index) {
         if (index + this.mapLayer.fullMapWidth < this.mapLayer.fullMapTileCount) {
-            return this.mapLayer.collisionArray[index + this.mapLayer.fullMapWidth];
+            return this.unusedCollisionArrayTiles[index + this.mapLayer.fullMapWidth];
         } else {
             return 0;
         }
