08b1af9afe052400258ed23efa459db2e8e5f218
brian zhou <brian.zhou@mail.mcgill.ca>
2016-03-29 11:28:52 -0400

    fixed boss

--- src/character/Boss.js
+++ src/character/Boss.js
@@ -7,7 +7,7 @@ var Boss = Character.extend({
      */
     ctor: function (space) {
         this.characterType = CHAR_TYPE.boss;
-        this._super(COLLISION_TYPE.enemy, new cc.PhysicsSprite(res.character_boss_png), ENEMY_STATS.baseHealth*8, ENEMY_STATS.baseHealthPoint*8, ENEMY_STATS.baseHitPoint*4, ENEMY_STATS.baseSpeed*2, ENEMY_STATS.baseSpeedDuration*0.5, ENEMY_STATS.inventoryCapacity, space, res.boss_plist, res.boss_png, "boss");
+        this._super(COLLISION_TYPE.enemy, new cc.PhysicsSprite(res.character_boss_png), ENEMY_STATS.baseHealth*8, ENEMY_STATS.baseHealthPoint*8, ENEMY_STATS.baseHitPoint*5, ENEMY_STATS.baseSpeed*2.5, ENEMY_STATS.baseSpeedDuration*0.5, ENEMY_STATS.inventoryCapacity, space, res.boss_plist, res.boss_png, "boss");
         this.projectileCount = 50;
     }
 });
