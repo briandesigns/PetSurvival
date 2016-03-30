1f606fa3e82c9b4a05d1bc26dfcba467f866e9dc
brian zhou <brian.zhou@mail.mcgill.ca>
2016-03-29 21:30:50 -0400

    changed player speed

--- src/play/PlayerLayer.js
+++ src/play/PlayerLayer.js
@@ -16,9 +16,9 @@ var PlayerLayer = cc.Layer.extend({
         this.player = new Player(character);
         this.init();
 
-        this._debugNode = new cc.PhysicsDebugNode(this.space);
-        this._debugNode.setVisible(true);
-        this.addChild(this._debugNode, 10);
+        //this._debugNode = new cc.PhysicsDebugNode(this.space);
+        //this._debugNode.setVisible(true);
+        //this.addChild(this._debugNode, 10);
 
     },
     init: function () {
