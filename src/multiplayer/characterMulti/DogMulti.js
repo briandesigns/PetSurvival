ddc8c7622619d3ff5dd10b05c46f7da7dfd5ff29
Joe Peplowski <jumpeplowski@gmail.com>
2016-04-13 20:56:58 -0400

    Fixed fast speed on single player

---
+++ src/multiplayer/characterMulti/DogMulti.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Joe on 2016-04-13.
+ */
+
+var DogMulti = Character.extend({
+    
+    /** Constructor
+     * @param {cc.SpriteBatchNode *}
+     * @param {cp.Space *}
+     * @param {cc.p}
+     */
+    ctor: function (space) {
+        this._super(COLLISION_TYPE.player, new cc.PhysicsSprite(res.character_dog_png), PLAYER_STATS.baseHealth, PLAYER_STATS.baseHealthPoint, PLAYER_MULTI_STATS.baseHitPoint, PLAYER_MULTI_STATS.baseSpeed, PLAYER_MULTI_STATS.baseSpeedDuration, PLAYER_MULTI_STATS.inventoryCapacity, space, res.dog_plist, res.dog_png, "dog");
+        this.characterType = CHAR_TYPE.dog;
+
+    }
+});
